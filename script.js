d8147e8bbc274c04ca5aaeadd5fc2241ee8b87c5
Novo <120156959+Novo1999@users.noreply.github.com>
2023-06-01 22:08:26 +0600

    ok

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ let count = parseFloat(quantity.textContent);
 const productImgSrc = document.querySelector('.main-img');
 const thumbnails = document.querySelectorAll('.product-items');
 const navItems = document.querySelectorAll('.nav-items');
+const cart = document.querySelector('.cart');
+const cartSection = document.querySelector('.cart-section');
+const emptyCart = document.querySelector('.empty');
+const cartItemCount = document.querySelector('.cart-item-count');
 
-console.log(productImgSrc);
 amount.addEventListener('click', e => {
   if (e.target.classList.contains('plus')) {
     count++;
@@ -29,3 +32,23 @@ thumbnails.forEach(img =>
     }
   })
 );
+cart.addEventListener('click', () => cartSection.classList.toggle('showCart'));
+
+addToCart.addEventListener('click', function cart() {
+  if (parseFloat(quantity.textContent) > 0) {
+    cartSection.insertAdjacentHTML(
+      'afterbegin',
+      `<h3>Cart</h3>
+      <hr class="cart-line">
+      <div class="cart-inner">
+      <img class="cart-img" src="/images/image-product-1-thumbnail.jpg" alt="">
+      <h2 class="cart-title">Fall Limited Edition Sneakers</h2>
+      <h4 class="cart-price">$125.00 x <span class="item-quantity"></span> 3 <span class="arrow">&rarr; </span><strong class="cart-total">$375.00</strong></h4>
+      <button class="delete">🗑️</button>
+      <button class="checkout">Checkout</button>
+      </div>`
+    );
+    addToCart.removeEventListener('click', cart);
+    emptyCart.remove();
+  }
+});
