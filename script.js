9bde0663c305cd68649fbcbbec615b01308c7d7e
Novo <120156959+Novo1999@users.noreply.github.com>
2023-06-02 10:59:24 +0600

    gallery done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addToCart = document.querySelector('button[data-value="cart"]');
 const amount = document.querySelector('.count');
 const quantity = document.querySelector('.quantity');
 const productImgSrc = document.querySelector('.main-img');
+const productImgSrc2 = document.querySelector('.main-img-2');
 const thumbnails = document.querySelectorAll('.product-items');
 const navItems = document.querySelectorAll('.nav-items');
 const cart = document.querySelector('.cart');
@@ -11,8 +12,16 @@ const cartItemCount = document.querySelector('.cart-item-count');
 const price = parseFloat(document.querySelector('.price-1').textContent);
 const cartInner = document.querySelector('.cart-inner');
 const showCart = document.querySelector('.showCart');
-console.log(price);
+const productName = document.querySelector('.product-name');
+const overlay = document.querySelector('.overlay');
+const gallery = document.querySelector('.product-gallery');
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
+const products = document.querySelectorAll('.p');
+console.log(products.length);
+console.log(thumbnails);
 let count = 0;
+let index = 1;
 amount.addEventListener('click', e => {
   if (e.target.classList.contains('plus')) {
     count++;
@@ -32,6 +41,8 @@ thumbnails.forEach(img =>
     for (let i = 1; i <= 4; i++) {
       if (e.target.classList.contains(`p-${i}`)) {
         productImgSrc.setAttribute('src', `/images/image-product-${i}.jpg`);
+      } else if (e.target.classList.contains(`d-${i}`)) {
+        productImgSrc2.setAttribute('src', `/images/image-product-${i}.jpg`);
       }
     }
   })
@@ -42,26 +53,52 @@ addToCart.addEventListener('click', function cart() {
   if (parseFloat(quantity.textContent) > 0) {
     cartSection.insertAdjacentHTML(
       'afterbegin',
-      `<h3>Cart</h3>
+      `<div class="added-section">
+      <h3>Cart</h3>
       <hr class="cart-line">
       <div class="cart-inner">
       <img class="cart-img" src="/images/image-product-1-thumbnail.jpg" alt="">
-      <h2 class="cart-title">Fall Limited Edition Sneakers</h2>
+      <h2 class="cart-title">${productName.textContent}</h2>
       <h4 class="cart-price">$125.00 x <span class="item-quantity"></span> ${count} <span class="arrow">&rarr; </span><strong class="cart-total">$${
         price * count
       }.00</strong></h4>
       <button class="delete">🗑️</button>
       <button class="checkout">Checkout</button>
+      </div>
       </div>`
     );
-    // addToCart.removeEventListener('click', cart);
     emptyCart.style.display = 'none';
     cartItemCount.innerHTML = `<h4 class="cartItem-amount">${count}</h4>`;
   }
 });
 
 cartSection.addEventListener('click', e => {
+  const addedElement = document.querySelector('.added-section');
   if (e.target.classList.contains('delete')) {
-    emptyCart.style.display = 'block';
+    addedElement.remove();
+    emptyCart.style.display = 'flex';
   }
+});
+
+productImgSrc.addEventListener('click', () => {
+  overlay.style.display = 'block';
+  gallery.style.display = 'block';
+  console.log(index);
+});
+
+rightBtn.addEventListener('click', () => {
+  index++;
+  if (index > products.length) {
+    index = 1;
+  }
+  productImgSrc2.setAttribute('src', `/images/image-product-${index}.jpg`);
+  console.log(index);
+});
+leftBtn.addEventListener('click', () => {
+  index--;
+  if (index < 1) {
+    index = products.length;
+  }
+  productImgSrc2.setAttribute('src', `/images/image-product-${index}.jpg`);
+  console.log(index);
 });
